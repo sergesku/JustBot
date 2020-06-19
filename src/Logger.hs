4be904adf03a49d548959b9b9aef5b4471f496e8
Sergey Kurgak <kurgak.ss@gmail.com>
2020-06-19 19:44:44 +1100

    add Priority data type

--- src/Logger.hs
+++ src/Logger.hs
@@ -1,1 +1,7 @@
 module Logger where
+
+data Priority = Debug     -- ^ Debug info
+              | Info      -- ^ Nottable information that requires no immediate action
+              | Warning   -- ^ Something is probably wrong and we should investigate
+              | Error     -- ^ Something is wrong and immediate action is required
+              deriving (Eq, Ord, Show)
