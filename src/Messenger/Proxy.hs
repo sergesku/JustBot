f7842cbb274ad34fffcf20b07e5f55b329f1fc84
Sergey Kurgak <kurgak.ss@gmail.com>
2020-06-20 23:49:12 +1100

    refact: hlint Messenger.Proxy

--- src/Messenger/Proxy.hs
+++ src/Messenger/Proxy.hs
@@ -2,7 +2,6 @@
 -- | It`s created for use in any instance of Bot_Handle. Each Bot_Handle implementation can use it`s own Proxy.
 
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -33,7 +32,7 @@ instance Monoid e => Applicative (Validate e) where
   Validate (Right f) <*> Validate a = Validate $ f <$> a
 
 instance Monoid e => MonadError e (Validate e) where
-  throwError e = Validate . Left $ e
+  throwError = Validate . Left
   Validate (Left e) `catchError` h = h e
   v `catchError` _ = v
 
@@ -41,13 +40,13 @@ proxyParser :: SectionParser (Maybe Proxy)
 proxyParser = fieldMbOf "proxy" $ \txt -> 
   let eProxy = runValidate $ validateProxy txt
    in case eProxy of
-        Left lst -> Left $ concat $ map T.unpack lst
+        Left lst -> Left $ concatMap T.unpack lst
         Right p  -> Right p
             
 validateProxy :: Text -> Validate [Text] Proxy
 validateProxy txt = 
   case T.words txt of
-   ip:port:[]   -> Proxy <$> validateIp ip <*> validatePort port
+   [ip,port]   -> Proxy <$> validateIp ip <*> validatePort port
    ip:port:rest -> throwError ["\nIncorrect Proxy format: " <> txt] <*> validateIp ip <*> validatePort port
    _            -> throwError ["\nIncorrect Proxy format: " <> txt]
 
@@ -60,12 +59,12 @@ validateIp ip = do
   let octs = T.splitOn "." ip
       result = traverse (checkOctet . T.unpack) octs
   case result of
-    Nothing -> throwError $ ["\nIncorrect IP format: " <> ip]
+    Nothing -> throwError ["\nIncorrect IP format: " <> ip]
     Just _  -> return $ T.encodeUtf8 ip
 
 validatePort :: Text -> Validate [Text] Int
 validatePort port = do
   let result = readMaybe (T.unpack port) >>= \p -> guard (p > 0) >> return p
   case result of
-    Nothing -> throwError $ ["\nIncorrect Port format: " <> port]
+    Nothing -> throwError ["\nIncorrect Port format: " <> port]
     Just p  -> return p
