3b6f4861dad74626a1377f5cd3c2595cf4e83900
Sergey Kurgak <kurgak.ss@gmail.com>
2020-06-21 20:52:48 +1100

    add: Database.IORefDB: logs

--- src/Database/IORefDB.hs
+++ src/Database/IORefDB.hs
@@ -3,6 +3,7 @@
 
 module Database.IORefDB where
 
+import qualified Logger
 import           Data.Singl
 import           Data.Update
 import           Database.Internal
@@ -22,22 +23,35 @@ getConfig singl txt = do
   Database{..} <- either (return . const emptyDB) getDatabase eFile
   Config <$> newIORef offset <*> newIORef userMap
   
-new :: Config -> IO Handle
-new Config{..} = return $ Handle{..} where
+new :: Config -> Logger.Handle -> IO Handle
+new Config{..} logH = return $ Handle{..} where
   getOffset :: IO Int
-  getOffset = readIORef offsetRef
+  getOffset = do
+    offset <- readIORef offsetRef
+    Logger.logDebug logH $ "Read IORef. Current offset: " <> show offset
+    return offset
 
-  setOffset :: Int -> IO ()
-  setOffset = writeIORef offsetRef
-  
   getUserRepeatN :: UserId -> IO (Maybe Int)
-  getUserRepeatN userId = M.lookup userId <$> readIORef userRef
+  getUserRepeatN userId = do
+    mbN <- M.lookup userId <$> readIORef userRef
+    let msg = case mbN of 
+                Just x  -> unwords ["Read IORef. User", show userId, ": number of repeats -", show x]
+                Nothing -> unwords ["Read IORef. User", show userId, "didn`t found in database."]
+    Logger.logDebug logH msg
+    return mbN
+  
+  setOffset :: Int -> IO ()
+  setOffset n = do
+    writeIORef offsetRef n
+    Logger.logDebug logH $ "Write IORef. New database offset: " <> show n
   
   setUserRepeatN :: UserId -> Int -> IO ()
   setUserRepeatN userId n = do
     m <- readIORef userRef
+    Logger.logDebug logH "Read user database from IORef."
     let newMap = M.insert userId n m
     writeIORef userRef newMap
+    Logger.logDebug logH $ unwords ["Write IORef. User", show userId, ": number of repeats -", show n]
 
-withHandle :: Config -> (Handle -> IO ()) -> IO ()
-withHandle cfg f = new cfg >>= f
+withHandle :: Config -> Logger.Handle -> (Handle -> IO ()) -> IO ()
+withHandle cfg logH f = new cfg logH >>= f
