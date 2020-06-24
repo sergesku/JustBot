fe058de67b9fe651165767b015eecb3854efb40e
Sergey Kurgak <kurgak.ss@gmail.com>
2020-06-24 12:49:12 +1100

    add: Data.Error: error types

--- src/Data/Error.hs
+++ src/Data/Error.hs
@@ -1,1 +1,27 @@
 module Data.Error where
+
+import Control.Exception
+import Data.Typeable
+
+data AppError = ErrorDB DatabaseError
+              | ErrorLog LogError
+              | ErrorMsg MessengerError
+              | ErrorCfg ConfigError
+              deriving (Show, Typeable)
+
+instance Exception AppError
+
+data DatabaseError = DatabaseReadError String
+                   | DatabaseWriteError String
+                   deriving Show
+
+data LogError = WriteLogError String
+                deriving Show
+
+data MessengerError = MessengerError deriving Show
+
+data ConfigError = MessengerConfigError String
+                | LoggingConfigError String
+                | DatabaseConfigError String
+                | ApplicationConfigError String
+                deriving Show
