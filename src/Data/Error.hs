70d17b4594717b4a3d350751903dfc81a726ea4a
Sergey Kurgak <kurgak.ss@gmail.com>
2020-07-11 12:53:31 +1100

    add: Data.Error: rethrowHTTPException

--- src/Data/Error.hs
+++ src/Data/Error.hs
@@ -2,8 +2,11 @@ module Data.Error where
 
 import Control.Exception
 import Data.Typeable
+import Network.HTTP.Simple
+import Network.HTTP.Client.Conduit
 
 data AppError = NetworkError SomeException
+              | MessengerResponseTimeout
               | SystemError SomeException
               | InputArgsError String
               | ConfigurationError String
@@ -13,8 +16,15 @@ data AppError = NetworkError SomeException
 instance Exception AppError
 
 instance Show AppError where
-  show (NetworkError e) = "Network communication error: " <> show e
+  show (NetworkError e) = "Network communication error:\n" <> show e
   show (SystemError e) = "System error: " <> show e
   show (InputArgsError t) = "Unsapported args: " <> t
   show (ConfigurationError t) = "Error parsing configuration file: " <> t
   show (ServiceApiError t) = "Error while communicating with external services: " <> t
+
+rethrowHTTPException :: HttpException -> IO a
+rethrowHTTPException (HttpExceptionRequest _ (StatusCodeException resp _)) = 
+  throw . ServiceApiError . show $ getResponseStatus resp
+rethrowHTTPException (HttpExceptionRequest _ ResponseTimeout) = 
+  throw MessengerResponseTimeout
+rethrowHTTPException e = throw . NetworkError $ toException e
