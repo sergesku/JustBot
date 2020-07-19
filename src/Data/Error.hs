29bcf689b6a89bdb8efc5846c7ebcd2f212a197e
Sergey Kurgak <kurgak.ss@gmail.com>
2020-07-19 15:38:45 +1100

    add Error: CodeError

--- src/Data/Error.hs
+++ src/Data/Error.hs
@@ -11,16 +11,19 @@ data AppError = NetworkError SomeException
               | InputArgsError String
               | ConfigurationError String
               | ServiceApiError String
+              | CodeError String
               deriving (Typeable)
 
 instance Exception AppError
 
 instance Show AppError where
+  show MessengerResponseTimeout = "Response timed out"
   show (NetworkError e) = "Network communication error:\n" <> show e
   show (SystemError e) = "System error: " <> show e
   show (InputArgsError t) = "Input args error:\n" <> t
   show (ConfigurationError t) = "Error parsing configuration file: " <> t
   show (ServiceApiError t) = "Error while communicating with external services: " <> t
+  show (CodeError t) = "Error in application code: " <> t
 
 rethrowHTTPException :: HttpException -> IO a
 rethrowHTTPException (HttpExceptionRequest _ (StatusCodeException resp _)) = 
