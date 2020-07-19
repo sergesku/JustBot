10e55c0c422899b267a3271d2d383391faa3fbbf
Sergey Kurgak <kurgak.ss@gmail.com>
2020-07-19 17:05:53 +1100

    refact: Data.Error: rethrowHTTPException

--- src/Data/Error.hs
+++ src/Data/Error.hs
@@ -11,7 +11,7 @@ data AppError = CodeError String
               | ServiceApiError String
               | ConfigurationError String
               | NetworkError SomeException
-              | MessengerResponseTimeout Int
+              | ResponseTimeoutError
               deriving (Typeable)
 
 instance Exception AppError
@@ -20,14 +20,16 @@ instance Show AppError where
   show (CodeError t) = "Error in application code: " <> t
   show (InputArgsError t) = "Input args error:\n" <> t
   show (SystemError e) = "System error: " <> show e
-  show (ServiceApiError t) = "Error while communicating with external services: " <> t
+  show (ServiceApiError t) = "Error while communicating with external services:\n" <> t
   show (ConfigurationError t) = "Error parsing configuration file: " <> t
   show (NetworkError e) = "Network communication error:\n" <> show e
-  show (MessengerResponseTimeout t) = "Response timed out: " <> show t <> " seconds."
+  show ResponseTimeoutError = "Messenger response timed out."
 
 rethrowHTTPException :: HttpException -> IO a
-rethrowHTTPException (HttpExceptionRequest _ (StatusCodeException resp _)) = 
-  throw . ServiceApiError . show $ getResponseStatus resp
-rethrowHTTPException (HttpExceptionRequest _ ResponseTimeout) = 
-  throw MessengerResponseTimeout
-rethrowHTTPException e = throw . NetworkError $ toException e
+rethrowHTTPException err = throw appError
+  where 
+    appError = case err of 
+      (InvalidUrlException url reason) -> ServiceApiError $ unwords [reason,"<", url, ">"]
+      (HttpExceptionRequest _ (StatusCodeException resp _)) -> ServiceApiError . show $ getResponseStatus resp
+      (HttpExceptionRequest _ ResponseTimeout) -> ResponseTimeoutError
+      e -> NetworkError $ toException e
