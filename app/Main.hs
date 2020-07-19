b37eab9fa02de939bda1d179ecce850eec857d7b
Sergey Kurgak <kurgak.ss@gmail.com>
2020-07-19 15:39:07 +1100

    refact: Main

--- app/Main.hs
+++ app/Main.hs
@@ -22,9 +22,13 @@ data AppOption where
   MsgOption :: SinglMsg m -> AppOption
   LogOption :: SinglLog l -> AppOption
 
+instance Show AppOption where
+  show (MsgOption m) = "MsgOption " <> show m
+  show (LogOption l) = "LogOption " <> show l 
+
 options :: [OptDescr AppOption]
-options = [ Option ['m'] ["messenger"] (ReqArg getMsgOption "Messenger") "Messenger to run the bot :: tg | vk"
-          , Option ['l'] ["log"] (ReqArg getLogOption "Log") "Log output :: console | file"
+options = [ Option ['m'] ["messenger"] (ReqArg readMsgOption "Messenger") "Messenger to run the bot :: tg | vk"
+          , Option ['l'] ["log"] (ReqArg readLogOption "Log") "Log output :: console | file"
           ]
 
 processStr :: String -> String
@@ -32,14 +36,14 @@ processStr = map toUpper . trim
   where trim = f . f
         f    = reverse . dropWhile isSpace
 
-getMsgOption :: String -> AppOption
-getMsgOption str = case processStr str of
+readMsgOption :: String -> AppOption
+readMsgOption str = case processStr str of
   "TG" -> MsgOption STG
   "VK" -> MsgOption SVK
   _    -> throwInputArgsError $ "Unsapported Messenger type: " <> str <> "."
 
-getLogOption :: String -> AppOption
-getLogOption str = case processStr str of
+readLogOption :: String -> AppOption
+readLogOption str = case processStr str of
   "FILE"    -> LogOption SFile
   "CONSOLE" -> LogOption SConsole
   _         -> throwInputArgsError $ "Unsapported Log type: " <> str <> "."
@@ -52,11 +56,13 @@ isLogOption :: AppOption -> Bool
 isLogOption (LogOption _) = True
 isLogOption _             = False
 
-withMsgOption :: AppOption -> (forall m. SinglMsg m -> r) -> r
-withMsgOption (MsgOption x) f = f x
+withMsg :: AppOption -> (forall m. SinglMsg m -> r) -> r
+withMsg (MsgOption m) f = f m
+withMsg x _ = throw $ CodeError $ "Main.withMsg called with unexpected argument < " <> show x <> " >"
 
-withLogOption :: AppOption -> (forall l. SinglLog l -> r) -> r
-withLogOption (LogOption l) f = f l
+withLog :: AppOption -> (forall l. SinglLog l -> r) -> r
+withLog (LogOption l) f = f l
+withLog x _ = throw $ CodeError $ "Main.withLog called with unexpected argument < " <> show x <> " >"
 
 getOption :: String -> (AppOption -> Bool) -> [AppOption] -> AppOption
 getOption opt f lst = case filter f lst of 
@@ -68,6 +74,18 @@ throwInputArgsError :: String -> a
 throwInputArgsError = throw . InputArgsError . withUsage
   where withUsage str = str <> "\n\n" <> usageInfo "Usage info: " options
 
+runChat :: SinglMsg m -> SinglLog l -> IO ()
+runChat msg logger = do
+  txt <- T.readFile "config.ini"
+  let Right logConfig = Log.getConfig logger txt
+  Log.withHandle logger logConfig $ \logH -> do
+    let Right config = getConfig txt
+    msgConfig <- MSG.getConfig msg logH txt
+    dbConfig  <- DB.getConfig msg logH txt
+    MSG.withHandle msg msgConfig logH $ \msgH ->
+      DB.withHandle dbConfig logH $ \dbH -> 
+        forever $ interaction msgH dbH config
+
 main = do
   args <- getArgs
   let (opts, nonOpts, unrec) = getOpt RequireOrder options args
@@ -75,16 +93,4 @@ main = do
       logOpt = getOption "Log" isLogOption opts
   unless (null nonOpts) $ throwInputArgsError $ unlines $ map ("unrecognized option: " <>) nonOpts
   unless (null unrec)   $ throwInputArgsError $ concat unrec
-  withLogOption logOpt $ withMsgOption msgOpt runChat
-
-runChat :: SinglMsg m -> SinglLog l -> IO ()
-runChat msg logger = do
-    txt <- T.readFile "config.ini"
-    let Right logConfig = Log.getConfig logger txt
-    Log.withHandle logger logConfig $ \logH -> do
-      let Right config = getConfig txt
-      msgConfig <- MSG.getConfig msg logH txt
-      dbConfig  <- DB.getConfig msg logH txt
-      MSG.withHandle msg msgConfig logH $ \msgH ->
-        DB.withHandle dbConfig logH $ \dbH -> 
-          forever $ interaction msgH dbH config
+  withLog logOpt $ withMsg msgOpt runChat
