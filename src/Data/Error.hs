851b122cb34283c08f7a73433331cc6e0859442e
Sergey Kurgak <kurgak.ss@gmail.com>
2020-07-19 15:46:09 +1100

    refact: Data.Error

--- src/Data/Error.hs
+++ src/Data/Error.hs
@@ -5,25 +5,25 @@ import Data.Typeable
 import Network.HTTP.Simple
 import Network.HTTP.Client.Conduit
 
-data AppError = NetworkError SomeException
-              | MessengerResponseTimeout
-              | SystemError SomeException
+data AppError = CodeError String
               | InputArgsError String
-              | ConfigurationError String
+              | SystemError SomeException
               | ServiceApiError String
-              | CodeError String
+              | ConfigurationError String
+              | NetworkError SomeException
+              | MessengerResponseTimeout Int
               deriving (Typeable)
 
 instance Exception AppError
 
 instance Show AppError where
-  show MessengerResponseTimeout = "Response timed out"
-  show (NetworkError e) = "Network communication error:\n" <> show e
-  show (SystemError e) = "System error: " <> show e
-  show (InputArgsError t) = "Input args error:\n" <> t
-  show (ConfigurationError t) = "Error parsing configuration file: " <> t
-  show (ServiceApiError t) = "Error while communicating with external services: " <> t
   show (CodeError t) = "Error in application code: " <> t
+  show (InputArgsError t) = "Input args error:\n" <> t
+  show (SystemError e) = "System error: " <> show e
+  show (ServiceApiError t) = "Error while communicating with external services: " <> t
+  show (ConfigurationError t) = "Error parsing configuration file: " <> t
+  show (NetworkError e) = "Network communication error:\n" <> show e
+  show (MessengerResponseTimeout t) = "Response timed out: " <> show t <> " seconds."
 
 rethrowHTTPException :: HttpException -> IO a
 rethrowHTTPException (HttpExceptionRequest _ (StatusCodeException resp _)) = 
