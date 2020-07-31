0d8801381dbad67b9a15e744174a694d507803d0
Sergey Kurgak <kurgak.ss@gmail.com>
2020-07-31 23:10:59 +1100

    refact: Main: getOptions

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE KindSignatures #-}
 
 module Main where
 
+import qualified Data.Map as M
+import           Data.Map (Map)
 import           Data.Error
 import           Control.Exception
 import           System.Console.GetOpt
@@ -22,11 +25,7 @@ data AppOption where
   MsgOption :: SinglMsg m -> AppOption
   LogOption :: SinglLog l -> AppOption
 
-instance Show AppOption where
-  show (MsgOption m) = "MsgOption " <> show m
-  show (LogOption l) = "LogOption " <> show l 
-
-options :: [OptDescr AppOption]
+options :: [OptDescr (Either String AppOption)]
 options = [ Option ['m'] ["messenger"] (ReqArg readMsgOption "Messenger") "Messenger to run the bot :: tg | vk"
           , Option ['l'] ["log"] (ReqArg readLogOption "Log") "Log output :: console | file"
           ]
@@ -36,17 +35,17 @@ processStr = map toUpper . trim
   where trim = f . f
         f    = reverse . dropWhile isSpace
 
-readMsgOption :: String -> AppOption
+readMsgOption :: String -> Either String AppOption
 readMsgOption str = case processStr str of
-  "TG" -> MsgOption STG
-  "VK" -> MsgOption SVK
-  _    -> throwInputArgsError $ "Unsapported Messenger type: " <> str <> "."
+  "TG" -> Right $ MsgOption STG
+  "VK" -> Right $ MsgOption SVK
+  _    -> Left  $ "Unsapported Messenger type: " <> str <> "."
 
-readLogOption :: String -> AppOption
+readLogOption :: String -> Either String AppOption
 readLogOption str = case processStr str of
-  "FILE"    -> LogOption SFile
-  "CONSOLE" -> LogOption SConsole
-  _         -> throwInputArgsError $ "Unsapported Log type: " <> str <> "."
+  "FILE"    -> Right $ LogOption SFile
+  "CONSOLE" -> Right $ LogOption SConsole
+  _         -> Left  $ "Unsapported Log type: " <> str <> "."
 
 isMsgOption :: AppOption -> Bool
 isMsgOption (MsgOption _ ) = True
@@ -56,26 +55,24 @@ isLogOption :: AppOption -> Bool
 isLogOption (LogOption _) = True
 isLogOption _             = False
 
-withMsg :: AppOption -> (forall m. SinglMsg m -> r) -> r
-withMsg (MsgOption m) f = f m
-withMsg x _ = throw $ CodeError $ "Main.withMsg called with unexpected argument < " <> show x <> " >"
-
-withLog :: AppOption -> (forall l. SinglLog l -> r) -> r
-withLog (LogOption l) f = f l
-withLog x _ = throw $ CodeError $ "Main.withLog called with unexpected argument < " <> show x <> " >"
-
-getOption :: String -> (AppOption -> Bool) -> [AppOption] -> AppOption
+getOption :: String -> (AppOption -> Bool) -> [AppOption] -> Either String AppOption
 getOption opt f lst = case filter f lst of 
-  [x] -> x
-  []  -> throwInputArgsError $ "You didn`t specify which " <> opt <> " to use."
-  _   -> throwInputArgsError $ "Too many " <> opt <> " options specified."
+  [x] -> Right x
+  []  -> Left $ "You didn`t specify which " <> opt <> " to use."
+  _   -> Left $ "Too many " <> opt <> " options specified."
 
-throwInputArgsError :: String -> a
-throwInputArgsError = throw . InputArgsError . withUsage
-  where withUsage str = str <> "\n\n" <> usageInfo "Usage info: " options
+extractOptions :: [String] -> Either String (AppOption, AppOption)
+extractOptions args = do
+  let (eOpts, nonOpts, unrec) = getOpt RequireOrder options args
+  unless (null nonOpts) $ Left $ unlines $ map ("unrecognized option: " <>) nonOpts
+  unless (null unrec)   $ Left $ concat unrec
+  opts   <- sequenceA eOpts
+  msgOpt <- getOption "Messenger" isMsgOption opts
+  logOpt <- getOption "Log" isLogOption opts
+  return (msgOpt, logOpt)
 
-runChat :: SinglMsg m -> SinglLog l -> IO ()
-runChat msg logger = do
+runChatWith :: SinglMsg m -> SinglLog l -> IO ()
+runChatWith msg logger = do
   txt <- T.readFile "config.ini"
   let Right logConfig = Log.getConfig logger txt
   Log.withHandle logger logConfig $ \logH -> do
@@ -86,11 +83,9 @@ runChat msg logger = do
       DB.withHandle dbConfig logH $ \dbH -> 
         forever $ interaction msgH dbH config
 
+main :: IO ()
 main = do
   args <- getArgs
-  let (opts, nonOpts, unrec) = getOpt RequireOrder options args
-      msgOpt = getOption "Messenger" isMsgOption opts
-      logOpt = getOption "Log" isLogOption opts
-  unless (null nonOpts) $ throwInputArgsError $ unlines $ map ("unrecognized option: " <>) nonOpts
-  unless (null unrec)   $ throwInputArgsError $ concat unrec
-  withLog logOpt $ withMsg msgOpt runChat
+  case extractOptions args of
+    Left str -> throw $ InputArgsError $ str <> "\n\n" <> usageInfo "Usage info: " options
+    Right (MsgOption m, LogOption l)    -> runChatWith m l
