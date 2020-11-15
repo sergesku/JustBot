7ae69918129380418a5f2c431e46c152a7ca084d
Sergey Kurgak <kurgak.ss@gmail.com>
2020-11-15 21:18:13 +1100

    refact: Error

--- src/Data/Error.hs
+++ src/Data/Error.hs
@@ -4,9 +4,9 @@ import Control.Exception
 import Data.Typeable
 import Network.HTTP.Simple
 import Network.HTTP.Client.Conduit
+import Data.Char (toUpper)
 
 data AppError = CodeError String
-              | InputArgsError ArgsError String
               | SystemError SomeException
               | ServiceApiError String
               | ConfigurationError String
@@ -24,7 +24,6 @@ instance Exception AppError
 
 instance Show AppError where
   show (CodeError t) = unlines ["Error in application code:", t]
-  show (InputArgsError e str) = unlines ["Input args error:", show e, str]
   show (SystemError e) = unlines ["System error:\n", show e]
   show (ServiceApiError t) = unlines ["Error while communicating with external services:\n", t]
   show (ConfigurationError t) = unlines ["Error parsing configuration file:", t]
@@ -32,11 +31,11 @@ instance Show AppError where
   show ResponseTimeoutError = "Messenger response timed out."
 
 instance Show ArgsError where
-  show (UnsapportedOption opt str) = "Unsapported " <> opt <> " type: " <> str <> "."
-  show (UnspecifiedOption opt) = "You didn`t specify which " <> opt <> " to use."
-  show (TooManyOptions opt) = "Too many " <> opt <> " options specified."
-  show (UnrecognizedOption str) = str
-  show (NonOption str) = "'" <> str <> "' is not a proper option"
+  show (UnsapportedOption opt str) = "Input args error: Unsapported " <> opt <> " type: '" <> str <> "'."
+  show (UnspecifiedOption opt) = "Input args error: You didn`t specify which " <> opt <> " to use."
+  show (TooManyOptions opt) = "Input args error: Too many " <> opt <> " options specified."
+  show (UnrecognizedOption str) = "Input args error: " <> headToUpper str
+  show (NonOption str) = "Input args error: '" <> str <> "' is not a proper option"
 
 rethrowHTTPException :: HttpException -> IO a
 rethrowHTTPException err = throwIO appError
@@ -49,3 +48,7 @@ rethrowHTTPException err = throwIO appError
 
 rethrowIOException :: IOException -> IO a
 rethrowIOException = throwIO . SystemError . toException
+
+headToUpper :: String -> String
+headToUpper [] = []
+headToUpper (x:xs) = toUpper x : xs
